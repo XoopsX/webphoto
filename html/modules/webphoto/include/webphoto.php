b60da223dd88fbf7eadf7a2749f8325f53cafb4b
Kenichi Ohwada <ml.ohwada@gmail.com>
2008-06-21 18:25:12 +0000

    v0.10

--- html/modules/webphoto/include/webphoto.php
+++ html/modules/webphoto/include/webphoto.php
@@ -1,16 +1,14 @@
 <?php
-// $Id: webphoto.php,v 1.1 2008/06/21 12:29:26 ohwada Exp $
+// $Id: webphoto.php,v 1.2 2008/06/21 18:25:12 ohwada Exp $
 
 //=========================================================
 // webphoto module
 // 2008-04-02 K.OHWADA
 //=========================================================
 
-if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'set XOOPS_TRUST_PATH into mainfile.php' ) ;
-
-$MY_DIRNAME = basename( dirname( dirname( __FILE__ ) ) );
-
-require XOOPS_ROOT_PATH.'/modules/'.$MY_DIRNAME.'/include/mytrustdirname.php' ; // set $mytrustdirname
-require XOOPS_TRUST_PATH.'/modules/'.$MY_TRUST_DIRNAME.'/include/waiting.plugin.php' ;
+//---------------------------------------------------------
+// this is the file for identifying webphoto module
+// do NOT remove this file
+//---------------------------------------------------------
 
 ?>
