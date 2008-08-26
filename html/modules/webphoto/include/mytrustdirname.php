8a7ca7f6c4bc08a6ac6274183f303dd806660ca5
Kenichi Ohwada <ml.ohwada@gmail.com>
2008-08-26 10:54:23 +0000

    v0.40

--- html/modules/webphoto/include/mytrustdirname.php
+++ html/modules/webphoto/include/mytrustdirname.php
@@ -1,19 +1,23 @@
 <?php
-// $Id: mytrustdirname.php,v 1.3 2008/08/25 19:53:34 ohwada Exp $
+// $Id: mytrustdirname.php,v 1.4 2008/08/26 10:54:23 ohwada Exp $
 
 //=========================================================
 // webphoto module
 // 2008-04-02 K.OHWADA
 //=========================================================
 
-// $mydirname = $MY_DIRNAME;
+//---------------------------------------------------------
+// change log
+// 2008-08-24 K.OHWADA
+// NOT use $mydirname
+//---------------------------------------------------------
 
 if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'not permit' ) ;
 
+//=========================================================
+// main
+//=========================================================
 $GLOBALS['MY_DIRNAME'] = $MY_DIRNAME;
-
-// for altsys
-// $mydirname = $MY_DIRNAME;
 
 $MY_TRUST_DIRNAME = 'webphoto' ;
 
