fd30e78abb10b5ae6f871d8364030b01f3c109ae
Kenichi Ohwada <ml.ohwada@gmail.com>
2010-03-14 17:14:27 +0000

    item_exif

--- xoops_trust_path/modules/webphoto/class/webphoto/exif.php
+++ xoops_trust_path/modules/webphoto/class/webphoto/exif.php
@@ -1,10 +1,16 @@
 <?php
-// $Id: exif.php,v 1.2 2009/01/24 15:33:44 ohwada Exp $
+// $Id: exif.php,v 1.3 2010/03/14 17:14:27 ohwada Exp $
 
 //=========================================================
 // webphoto module
 // 2009-01-10 K.OHWADA
 //=========================================================
+
+//---------------------------------------------------------
+// change log
+// 2010-03-14 K.OHWADA
+// webphoto_lib_multibyte
+//---------------------------------------------------------
 
 if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'not permit' ) ;
 
@@ -16,16 +22,19 @@ class webphoto_exif
 {
 	var $_exif_class;
 	var $_utility_class;
+	var $_multibyte_class;
 
 	var $_GMAP_ZOOM = _C_WEBPHOTO_GMAP_ZOOM ;
+	var $_EXIF_LENGTH = 65000 ; // 64KB
 
 //---------------------------------------------------------
 // constructor
 //---------------------------------------------------------
 function webphoto_exif()
 {
-	$this->_exif_class    =& webphoto_lib_exif::getInstance();
-	$this->_utility_class =& webphoto_lib_utility::getInstance();
+	$this->_exif_class      =& webphoto_lib_exif::getInstance();
+	$this->_utility_class   =& webphoto_lib_utility::getInstance();
+	$this->_multibyte_class =& webphoto_lib_multibyte::getInstance();
 }
 
 function &getInstance()
@@ -67,7 +76,7 @@ function build_row_exif( $row, $file )
 		$row['item_gmap_zoom']      = $this->_GMAP_ZOOM ;
 	}
 	if ( $exif ) {
-		$row['item_exif'] = $exif ;
+		$row['item_exif'] = $this->format_exif( $exif ) ;
 		$flag = 2 ;
 	}
 
@@ -91,6 +100,17 @@ function exif_to_mysql_datetime( $exif )
 	if ( $time <= 0 ) { return false; }
 
 	return $this->time_to_mysql_datetime( $time );
+}
+
+function format_exif( $str )
+{
+	if ( strlen($str) < $this->_EXIF_LENGTH ) {
+		return $str ;
+	}
+
+	$str = $this->_multibyte_class->convert_encoding( $str, 'ASCII', 'UTF-8' );
+	$str = substr( $str, 0, $this->_EXIF_LENGTH );
+	return $str;
 }
 
 function get_row()
