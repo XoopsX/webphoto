fcb126791d2440bbec17c709f6d857619ec68be7
Kenichi Ohwada <ml.ohwada@gmail.com>
2010-04-03 23:26:01 +0000

    not show embed

--- xoops_trust_path/modules/webphoto/main/flash_config.php
+++ xoops_trust_path/modules/webphoto/main/flash_config.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: flash_config.php,v 1.2 2008/12/18 13:23:16 ohwada Exp $
+// $Id: flash_config.php,v 1.3 2010/04/03 23:26:01 ohwada Exp $
 
 //=========================================================
 // webphoto module
@@ -8,6 +8,8 @@
 
 //---------------------------------------------------------
 // change log
+// 2010-04-04 K.OHWADA
+// class/webphoto/base_ini.php
 // 2008-12-12 K.OHWADA
 // header_file.php
 //---------------------------------------------------------
@@ -27,6 +29,7 @@ webphoto_include_once( 'class/lib/remote_file.php' );
 webphoto_include_once( 'class/lib/xml.php' );
 webphoto_include_once( 'class/handler/player_handler.php' );
 webphoto_include_once( 'class/handler/flashvar_handler.php' );
+webphoto_include_once( 'class/webphoto/base_ini.php' );
 webphoto_include_once( 'class/webphoto/playlist.php' );
 webphoto_include_once( 'class/webphoto/flash_player.php' );
 webphoto_include_once( 'class/main/flash_config.php' );
