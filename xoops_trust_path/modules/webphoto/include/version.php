515b024f0d5e08a18f8a328c326c3b773874817d
Kenichi Ohwada <ml.ohwada@gmail.com>
2011-05-16 02:27:21 +0000

    v2.41

--- xoops_trust_path/modules/webphoto/include/version.php
+++ xoops_trust_path/modules/webphoto/include/version.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: version.php,v 1.39 2011/05/10 02:56:39 ohwada Exp $
+// $Id: version.php,v 1.40 2011/05/16 02:27:21 ohwada Exp $
 
 //=========================================================
 // webphoto module
@@ -11,7 +11,7 @@ if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'not permit' ) ;
 // --- define version begin ---
 if( !defined("_C_WEBPHOTO_VERSION") ) 
 {
-	define("_C_WEBPHOTO_VERSION", "2.40" ) ;
+	define("_C_WEBPHOTO_VERSION", "2.41" ) ;
 	define("_C_WEBPHOTO_TIMELINE_VERSION", "0.11" ) ;
 }
 // --- define version end ---
